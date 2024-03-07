0f129daa2f3991b685eff4525628f281cea06550
Abubakar1010 <abubakar850772@gmail.com>
2024-03-07 18:36:36 +0600

    makes blocks using class

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
 const container = document.getElementById('container')
-// const block = document.getElementById('block')
+// const block = document.getElementById('block'h
+
+const blockHeight = 20;
+const blockWidth = 100;
+// make blocks class
+
+class Block{
+
+    constructor(xAxis,yAxis){
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.bottomLeft = [xAxis, yAxis]
+        
+    }
+}
+
+// all blocks 
+
+const blocks = [
+        new Block(10, 270),
+        new Block(120, 270),
+        new Block(230, 270),
+        new Block(340, 270),
+        new Block(450, 270),
+        new Block(10, 230),
+        new Block(120, 230),
+        new Block(230, 230),
+        new Block(340, 230),
+        new Block(450, 230),
+        new Block(10, 190),
+        new Block(120, 190),
+        new Block(230, 190),
+        new Block(340, 190),
+        new Block(450, 190),
+    ]
+
+console.log(blocks[0]);
 
 function addBlock(){
 
+    blocks.forEach( function(element){
     const block = document.createElement('div')
 
-    block.classList.add('block')
+        block.classList.add('block')
 
-    block.style.left = '100px'
-    block.style.bottom = '50px'
+        block.style.left = element.bottomLeft[0] + 'px'
+        block.style.bottom =  element.bottomLeft[1] + 'px'
 
-    container.appendChild(block)
+        console.log(element.bottomLeft[0]);
+        container.appendChild(block)
+
+    })
 }
 
 addBlock()
